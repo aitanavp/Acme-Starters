d1239443208f315fc50eaf7c669b829faf219712
Alberto <littlegamesetc@gmail.com>
2026-02-16 00:06:59 +0100

    data model C03 implemented

---
+++ src/main/java/acme/entities/strategies/Tactic.java
@@ -0,0 +1,42 @@
+
+package acme.entities.strategies;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidScore;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tactic extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes 
+
+	@Mandatory
+	//@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	//@ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidScore
+	@Column
+	private Double				expectedPercentage;
+
+	@Mandatory
+	@Valid
+	@Column
+	private TacticKind			kind;
+}
