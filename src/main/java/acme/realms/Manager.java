d4b974ee0f44ede6fd72ce8d26f07df5db5eacc7
aitanavp <aitvalpra@alum.us.es>
2026-04-27 08:54:47 +0200

    Add role Manager

---
+++ src/main/java/acme/realms/Manager.java
@@ -0,0 +1,41 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.validation.Mandatory;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Manager extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				position;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				skills;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				isExecutive;
+
+}
